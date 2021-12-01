5364e6f2e580946d77b1e86b45a93c45495fe8a1
Bagay Valery <lerika1307@gmail.com>
2021-12-01 22:37:48 +0300

    Schedule Model v.1.0

---
+++ Model/Enity/Schedule.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Enity
+{
+    public class Schedule
+    {
+
+        private string NameOfFedder;
+        /// <summary>
+        /// Количество корма по расписанию
+        /// </summary>
+        private int MountOfFeed { get; set; }
+        /// <summary>
+        /// Массив времени кормёжки по расписанию
+        /// </summary>
+        private List<DateTime> TimeToFeed { get; set; }
+
+        public Schedule(DateTime timeTofeed, int mountOffeed)
+        {
+            this.TimeToFeed.Add(timeTofeed);
+            this.MountOfFeed = mountOffeed;
+        }
+
+        public Schedule()
+        {
+            this.MountOfFeed = 0;
+            this.TimeToFeed.Clear();
+        }
+    }
+}
---
+++ Model/IShceduleService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    interface IShceduleService
+    {
+        /// <summary>
+        /// Выбор расписания
+        /// </summary>
+        public void ChooseSchedule();
+
+    }
+}
