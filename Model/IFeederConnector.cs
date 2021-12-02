f7d8839dad2086470d0383761bfca644d9b990f1
Bagay Valery <lerika1307@gmail.com>
2021-12-02 15:59:42 +0300

    Schedule Model v1.0

---
+++ Model/IFeederConnector.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    interface IFeederConnector
+    {
+        void FeedNow(int amount);
+        void SetSchedule(/*Schedule link*/);
+        void GetSchedule();
+    }
+}
